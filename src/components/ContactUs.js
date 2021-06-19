d3d9f9de7c14542e95423376e0569b7d8f463637
captain-bari <barinder.turkey@gmail.com>
2021-06-20 04:28:53 +0530

    margin theek kerdi

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -20,12 +20,17 @@ export default function ContactUs() {
     const [mail, setMail] = useState("");
     const [message, setMessage] = useState("");
     const [value, setValue] = useState()
+    const [value2, setValue2] = useState("")
     const [open, setOpen] = React.useState(false)
+    const [sent, setsent] = useState(false)
 
     var data = {
         to_email:"barinder.turkey",
         from_name:mail,
-        message :message
+        from_subject:"",
+        from_date : "",
+        mobile : "",
+        remark : ""
       };
 
 
@@ -36,6 +41,25 @@ export default function ContactUs() {
     }, (error) => {
       console.log(error.text);
     });
+}
+const changeDate = (event) => {
+  setValue2(event.toDate())
+}
+const send = () => {
+  if(sent == false){
+    data.mobile = value;
+    data.from_name = document.getElementById("basic-email").value;
+    data.from_subject = document.getElementById("basic-subject").value;
+    data.from_date = value2;
+    data.remark = document.getElementById("basic-remark").value;
+    emailjs.send('service_5zxqe2p', 'template_2607eqo', data , 'user_0nhOQbYFrKQvGH84WriZP')
+    .then((result) => {
+      console.log(result.status , result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+    setsent(true);
+  }
 }
 
    
@@ -55,24 +79,24 @@ export default function ContactUs() {
       <Col xs={3}><InputGroup.Text id="basic-addon3">
       Email address
       </InputGroup.Text></Col>
-      <Col Col xs={6}> <FormControl id="basic-url" aria-describedby="basic-addon3" placeholder='Enter Email Address'  className="smaller-input"/></Col><Col></Col>
+      <Col Col xs={6}> <FormControl id="basic-email" aria-describedby="basic-addon3" placeholder='Enter Email Address'  className="smaller-input"/></Col><Col></Col>
     </Row><Row><br></br></Row>
      
     <Row>
       <Col xs={3}><InputGroup.Text id="basic-addon3">
         Subject Name
       </InputGroup.Text></Col>
-      <Col Col xs={6}><FormControl id="basic-url" aria-describedby="basic-addon3" /></Col>
+      <Col Col xs={6}><FormControl id="basic-subject" aria-describedby="basic-addon3" /></Col>
     </Row><Row><br></br></Row>
    
   <Row><Col xs={3}> <InputGroup.Text id="basic-addon3">
         Book Slot here
-      </InputGroup.Text></Col><Col Col xs={6}> <Datetime placeholder='book'/></Col></Row><Row><br></br></Row>
+      </InputGroup.Text></Col><Col Col xs={6}> <Datetime id="date-time" onChange={changeDate} /></Col></Row><Row><br></br></Row>
       <Row>
       <Col xs={3}><InputGroup.Text id="basic-addon3">
         Remarks
       </InputGroup.Text></Col>
-      <Col Col xs={6}><FormControl id="basic-url" aria-describedby="basic-addon3" /></Col>
+      <Col Col xs={6}><FormControl id="basic-remark" aria-describedby="basic-addon3" /></Col>
     </Row>
       <Form.Text className="disclamer">
       ** Your details are safe with us. Here, on The Toppers Club, We respect your Privacy. Our subject experts will contact you personally on the Whatsapp number provided.
@@ -80,11 +104,13 @@ export default function ContactUs() {
 </Form><Row><br></br></Row>
 <Modal
       basic
+      onClick = {send}
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
       open={open}
       size='fullscreen'
       trigger={<Button primary>Submit</Button>}
+      
     >
       <Header icon>
         <Icon name='archive' />
